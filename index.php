513b02fbba017f1035ce6cf7429e3b874ff4a40f
singh-abhijit <itzabhijits@gmail.com>
2020-06-29 21:22:02 +0530

    fixed 500 Server Error

--- index.php
+++ index.php
@@ -11,16 +11,13 @@
             <h1 class="" style="color : white">Food Shala</h1>
             <h2 class="" style="color : seashell">Feeling Hungry ?</h2>
             <h2 class="" style="color : seashell">Order food from Foodshala.</h2>
-            <button type="button" class="btn btn-light btn_order_now" onclick="<?php header('location : menu.php') ?>">Order Now</button>
+            <a href="menu.php" class=""><button type="button" class="btn btn-light btn_order_now" id="take_to_menu.php">Order Now</button></a>
         </div>
-
     </div>
     <?php
     require_once('./components/footer.php');
     require_once('./components/extras.php');
     ?>
-    <!--Extras  -->
-
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
