7d5a6c8aa71eca5935e4c1f378e4e5b5253f9fae
singh-abhijit <itzabhijits@gmail.com>
2020-07-10 19:01:11 +0530

    dbconfig update

--- dbconfig/config.php
+++ dbconfig/config.php
@@ -1,6 +1,6 @@
 <?php
 //$con = mysqli_connect("localhost", "root", "") or die('Can\'t establish a database connection');
 //mysqli_select_db($con, 'foodshaladb');
-$con = mysqli_connect("d9c88q3e09w6fdb2.cbetxkdyhwsb.us-east-1.rds.amazonaws.com", "l3lsv4q4cfs3liqr", "q991t4jsxiw0zzqf") or die('Can\'t establish a database connection');
-mysqli_select_db($con, 'yj3q0vxvbwkmsws2');
+$con = mysqli_connect("d9c88q3e09w6fdb2.cbetxkdyhwsb.us-east-1.rds.amazonaws.com", "mcqtn4po16fswsmn", "vto2tv2b8rm3e611") or die('Can\'t establish a database connection');
+mysqli_select_db($con, 'h2qvja1tni5r0lxk');
 ?>
