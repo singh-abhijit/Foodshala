9bbef24eb69bd2f36c1f5f6499a6513852ac7676
singh-abhijit <itzabhijits@gmail.com>
2020-07-10 19:16:32 +0530

    login messgae

--- login.php
+++ login.php
@@ -10,63 +10,65 @@ require_once('dbconfig/config.php');
 <body>
     <?php require_once('./components/header.php') ?>
     <div class="body_and_footer_container my-auto" id='login-body' style="min-height : 62%">
-    <br >
-    <br >
+        <br>
+        <br>
         <!-- <div class="body_main"> -->
         <div class="card" style="width : 25%; margin : auto; margin-bottom: 2%; margin-top : 6%; margin-right : 17%; min-width : 240px">
-                <div class="card-body">
+            <div class="card-body">
 
-                    <form action="login.php" method="post">
-                        <div class="form-group">
-                            <label for="inputUsername">Username</label>
-                            <input type="text" class="form-control" id="inputUsername" name='username' required>
-                        </div>
-                        <div class="form-group">
-                            <label for="inputPassword">Password</label>
-                            <input type="password" class="form-control" id="inputPassword" name='password' required>
-                        </div>
-                        <button type="submit" class="btn btn-primary" name="login">Login</button>
-                    </form>
+                <form action="login.php" method="post">
+                    <div class="form-group">
+                        <label for="inputUsername">Username</label>
+                        <input type="text" class="form-control" id="inputUsername" name='username' required>
+                    </div>
+                    <div class="form-group">
+                        <label for="inputPassword">Password</label>
+                        <input type="password" class="form-control" id="inputPassword" name='password' required>
+                    </div>
+                    <button type="submit" class="btn btn-primary" name="login">Login</button>
+                </form>
 
 
-                </div>
             </div>
+        </div>
 
-            <?php
-            if (isset($_POST['login'])) {
-                @$username = $_POST['username'];
-                @$password = $_POST['password'];
-                $query_customers = "select * from customers where username='$username' and password='$password' ";
-                $query_restaurants = "select * from restaurants where restaurant_username='$username' and restaurant_password='$password' ";
+        <?php
+        if (isset($_POST['login'])) {
+            @$username = $_POST['username'];
+            @$password = $_POST['password'];
+            $query_customers = "select * from customers where username='$username' and password='$password' ";
+            $query_restaurants = "select * from restaurants where restaurant_username='$username' and restaurant_password='$password' ";
 
-                $query_customers_run = mysqli_query($con, $query_customers);
-                $query_restaurants_run = mysqli_query($con, $query_restaurants);
+            $query_customers_run = mysqli_query($con, $query_customers);
+            $query_restaurants_run = mysqli_query($con, $query_restaurants);
 
-                if ($query_customers_run || $query_restaurants_run) {
-                    if (mysqli_num_rows($query_customers_run) > 0) {
-                        $row = mysqli_fetch_array($query_customers_run, MYSQLI_ASSOC);
+            if ($query_customers_run || $query_restaurants_run) {
+                if (mysqli_num_rows($query_customers_run) > 0) {
+                    $row = mysqli_fetch_array($query_customers_run, MYSQLI_ASSOC);
 
-                        $_SESSION['username'] = $username;
-                        $_SESSION['name'] = $row['name'];
-                        $_SESSION['user_type'] = "customer";
-                        header("Location: index.php");
-                    }
-                    if (mysqli_num_rows($query_restaurants_run) > 0) {
-                        $row = mysqli_fetch_array($query_restaurants_run, MYSQLI_ASSOC);
+                    $_SESSION['username'] = $username;
+                    $_SESSION['name'] = $row['name'];
+                    $_SESSION['user_type'] = "customer";
+                    echo '<script type="text/javascript">alert("Welcome ' . $_SESSION['name'] . ' ")</script>';
+                    header("Location: index.php");
 
-                        $_SESSION['username'] = $username;
-                        $_SESSION['name'] = $row['restaurant_name'];
-                        $_SESSION['user_type'] = "restaurant";
-                        header("Location: index.php");
-                    } else {
-                        echo '<script type="text/javascript">alert("No such User exists. Invalid Credentials. Please Try again")</script>';
-                    }
+                } elseif (mysqli_num_rows($query_restaurants_run) > 0) {
+                    $row = mysqli_fetch_array($query_restaurants_run, MYSQLI_ASSOC);
+
+                    $_SESSION['username'] = $username;
+                    $_SESSION['name'] = $row['restaurant_name'];
+                    $_SESSION['user_type'] = "restaurant";
+                    echo '<script type="text/javascript">alert("Welcome ' . $_SESSION['name'] . ' ")</script>';
+                    header("Location: index.php");
                 } else {
-                    echo '<script type="text/javascript">alert("Database error. Please check the conncetion")</script>';
+                    echo '<script type="text/javascript">alert("No such User exists. Invalid Credentials. Please Try again")</script>';
                 }
             } else {
+                echo '<script type="text/javascript">alert("Database error. Please check the conncetion")</script>';
             }
-            ?>
+        } else {
+        }
+        ?>
         <!-- </div> -->
     </div>
     <?php
